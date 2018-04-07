5c2e0e9a84b63ecb8496ef5c0f65b6c030e07d7a
Hunter <mcpadden.hunter@gmail.com>
2018-04-07 17:57:00 -0400

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import SocialLinks from "./SocialLinks";
+
+const Banner = () => (
+  <div className="row banner">
+    <div className="banner-text">
+      <h1 className="responsive-headline">I'm Hunter McPadden.</h1>
+      <h3>
+        I'm a Washington DC based <span>developer</span>,
+        <span> cloud architect</span> and <span>entrepreneur</span> creating awesome
+        and effective visual identities for companies of all sizes around the
+        globe. Let's
+        <a className="smoothscroll" href="#about">
+          {" "}
+          start scrolling
+        </a>{" "}
+        and learn more
+        <a className="smoothscroll" href="#about">
+          {" "}
+          about me
+        </a>.
+      </h3>
+      <hr />
+      <SocialLinks />
+    </div>
+  </div>
+);
+
+export default Banner;
---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Link from "gatsby-link";
+
+import "../assets/css/main.css";
+
+import Header from "../components/Header";
+import About from "../components/About";
+import Timeline from "../components/Timeline";
+import Resume from "../components/Resume";
+import Portfolio from "../components/Portfolio";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
+
+const IndexPage = () => (
+  <div>
+    <Header />
+    <About />
+    <Timeline />
+    <Resume />
+    <Portfolio />
+    <Testimonials />
+    <Footer />
+  </div>
+);
+
+export default IndexPage;
